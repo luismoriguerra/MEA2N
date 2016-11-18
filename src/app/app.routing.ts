753775c4a1df69d6afe9af7f82825d935b269144
luis mori <luismori@MacBook-Pro-de-luis.local>
2016-11-18 01:43:26 -0500

    base project v0.4.2 cleanning

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { Zones3Component } from './zones3/zones3.component';
+
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'zone3', component: Zones3Component}
+  { path: '', component: HomeComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
